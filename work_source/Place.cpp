55d61628dd62060841f27f83810036771b1c6048
Josh Landron <jlandron@uw.edu>
2019-07-21 22:56:42 -0700

    started to implement neighbors functionality

--- work_source/Place.cpp
+++ work_source/Place.cpp
@@ -9,22 +9,53 @@
  furnished to do so, subject to the following conditions:
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- The following acknowledgment shall be used where appropriate in publications, presentations, etc.:
- © 2014-2015 University of Washington. MASS was developed by Computing and Software Systems at University of Washington Bothell.
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+ The following acknowledgment shall be used where appropriate in publications,
+ presentations, etc.: © 2014-2015 University of Washington. MASS was developed
+ by Computing and Software Systems at University of Washington Bothell. THE
+ SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include "Place.h"
-#include "MASS_base.h"
 #include <iostream>
+#include "MASS_base.h"
 
-
-vector<int> Place::getSizeVect(  ){
-	return size;
+vector<int> Place::getSizeVect() { return size; }
+/**
+ * clears the neighbor vector
+ */
+void Place::cleanNeighbors() { neighbors.clear(); }
+/**
+ * replace neighbors completely using a vector of places
+ */
+void Place::addNeighbors(vector<Place *> places) {
+    neighbors.clear();
+    for (int i = 0; i < places.size(); i++) {
+        neighbors.push_back(places.at(i));
+    }
+}
+/**
+ * Add a single neighbor to this place
+ */
+void Place::addNeighbor(Place *place) { neighbors.push_back(place); }
+/**
+ * adds neighbors in a given pattern if they exist.
+ */
+void Place::addNeighbors(neighborPattern pattern) {
+    switch (pattern) {
+        case FOUR: {
+            // TODO implement indexing
+            break;
+        }
+        case EIGHT: {
+            // TODO implement indexing
+            break;
+        }
+        default:
+            break;
+    }
 }
--- work_source/Place.h
+++ work_source/Place.h
@@ -9,45 +9,65 @@
  furnished to do so, subject to the following conditions:
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- The following acknowledgment shall be used where appropriate in publications, presentations, etc.:
- © 2014-2015 University of Washington. MASS was developed by Computing and Software Systems at University of Washington Bothell.
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+ The following acknowledgment shall be used where appropriate in publications,
+ presentations, etc.: © 2014-2015 University of Washington. MASS was developed
+ by Computing and Software Systems at University of Washington Bothell. THE
+ SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef PLACE_H
 #define PLACE_H
 
-#include "MObject.h"
-#include <vector>
 #include <iostream>
+#include <vector>
+#include "MObject.h"
 
 using namespace std;
 
 class Place : MObject {
-  friend class Places_base;
-  friend class Agents_base;
-  friend class Agent;
- public:
-  Place( void *argument ) : outMessage( NULL ), outMessage_size( 0 ), 
-    inMessage_size( 0 ) { inMessages.clear( ); };
- 
-  vector<int> getSizeVect( );
-  virtual void *callMethod( int functionId, void *argument ) = 0;
-  ~Place( ) { };
+    friend class Places_base;
+    friend class Agents_base;
+    friend class Agent;
 
-  vector<int> size;
-  vector<int> index;
-  void *outMessage;
-  vector<void*> inMessages;
-  int outMessage_size;
-  int inMessage_size;
-  vector<MObject*> agents;
+   public:
+    Place(void *argument)
+        : outMessage(NULL), outMessage_size(0), inMessage_size(0) {
+        inMessages.clear();
+    };
+    // rename patterns according to specifications
+    enum neighborPattern { FOUR, EIGHT };
+
+    vector<int> getSizeVect();
+    virtual void *callMethod(int functionId, void *argument) = 0;
+    ~Place(){};
+
+    vector<int> size;
+    vector<int> index;
+    void *outMessage;
+    vector<void *> inMessages;
+    int outMessage_size;
+    int inMessage_size;
+    vector<MObject *> agents;
+    vector<Place *> neighbors;
+
+    void cleanNeighbors();
+    void addNeighbor(Place *place);
+    void addNeighbors(vector<Place *>);
+    void addNeighbors(neighborPattern pattern);
+
+    // add vector for neighbors, implement in places_base, exchange all recives
+    // neighboring information, make new exchange all that uses neighbors vector
+    // instead of passed information
+
+    // cleanNeighbors()
+    // addNeighbors(vector)
+    // addNeighbors(Enumeration of different neighbor patterns)
+    // exchange all implementation in places basea
 };
 
 #endif
