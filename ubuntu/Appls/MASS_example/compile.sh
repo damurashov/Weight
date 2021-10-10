7d8e9f0557a4b8f575c32698e0f212d8a650f571
DmitryMurashov <dmtr.murashov@gmail.com>
2021-10-10 10:58:55 +0300

    [project] Cleanup
    - Gathered docs in one place
    - Removed/merged intermediate folders. Moved the isolated environment created within ubuntu/ directory into the root
    - Fixed files organization
    - Removed ssh2 that used to be placed into project directory structure as a subdirectory. Probably, it will be added as a git submodule some time later.

--- ubuntu/Appls/MASS_example/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#export MASS_DIR=/net/metis/home3/dslab/MASS/c++
-
-export MASS_DIR=../../../
-
-g++ -Wall Nomad.cpp -I$MASS_DIR/source -shared -fPIC -o Nomad
-g++ -Wall Land.cpp -I$MASS_DIR/source -shared -fPIC -o Land
-g++ -Wall main.cpp -I$MASS_DIR/source -L$MASS_DIR/ubuntu -lmass -I$MASS_DIR/ubuntu/ssh2/include -L$MASS_DIR/ubuntu/ssh2/lib -lssh2 -o main
